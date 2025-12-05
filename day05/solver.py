bd3c5a1c262ab605b5a93d657d55baf4bc2a4539
Tom Gagnier <tomgagnier@gmail.com>
2025-12-05 17:48:13 -0500

    refactor day5

--- day05/solver.py
+++ day05/solver.py
@@ -1,4 +1,4 @@
-def merge(ranges: list[range]) -> list[range]:
+def sort_merge(ranges: list[range]) -> list[range]:
     result: list[range] = []
     for r in sorted(ranges, key=lambda x: x.start):
         if result and result[-1].stop >= r.start:
@@ -7,20 +7,25 @@ def merge(ranges: list[range]) -> list[range]:
             result.append(r)
     return result
 
+
 def solve(file_name: str):
     with (open(file_name) as f):
         paragraphs = [text.splitlines() for text in f.read().split('\n\n')]
 
-    fresh_ingredient_ranges = merge(
+    fresh_ingredient_ranges = sort_merge(
         [range(start, end + 1)
          for start, end in (map(int, r.split('-')) for r in paragraphs[0])]
     )
 
     ingredients = sorted(map(int, paragraphs[1]))
 
+    part1 = sum(1 for i in ingredients if any(i in r for r in fresh_ingredient_ranges))
+
+    part2 = sum(r.stop - r.start for r in fresh_ingredient_ranges)
+
     print(file_name)
-    print('    part1', sum(1 for i in ingredients if any(i in r for r in fresh_ingredient_ranges)))
-    print('    part2', sum(r.stop - r.start for r in fresh_ingredient_ranges))
+    print('  part1', part1)
+    print('  part2', part2)
 
 
 solve("example.txt")
